05242e96b651be7033eb1e44254823c0b494df27
Darko Palic <darko.palic@xenovation.com>
2011-04-06 14:30:58 +0000

    merged from 3.6-branch

---
+++ dromdary.common.generator/src/main/java/org/dromdary/common/generator/helper/CommonPropertiesHelper.java
@@ -0,0 +1,23 @@
+package org.dromdary.common.generator.helper;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class CommonPropertiesHelper {
+	
+	public static String COMMON_PROPERTIES_PATH = "src/main/resources/common.properties";
+	public static String PROPERTY_GENERATE_JPA = "generate.jpa";
+	public static String PROPERTY_GENERATE_BVA = "generate.bva";
+	
+	public static String getJpaFlag() throws FileNotFoundException, IOException {
+		Properties properties = new Properties();
+		properties.load(new FileInputStream(COMMON_PROPERTIES_PATH));
+		return properties.getProperty(PROPERTY_GENERATE_JPA);
+	}
+	public static String getBvaFlag() throws FileNotFoundException, IOException {
+		Properties properties = new Properties();
+		properties.load(new FileInputStream(COMMON_PROPERTIES_PATH));
+		return properties.getProperty(PROPERTY_GENERATE_BVA);
+	}
+}
