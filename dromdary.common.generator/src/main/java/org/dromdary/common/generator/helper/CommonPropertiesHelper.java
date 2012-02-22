c598a76dad63851a7ef29dd801e735df1565696a
Roman Scharton <roman.scharton@t-systems.com>
2012-02-22 13:33:23 +0000

    property for not_entity_classes support

--- dromdary.common.generator/src/main/java/org/dromdary/common/generator/helper/CommonPropertiesHelper.java
+++ dromdary.common.generator/src/main/java/org/dromdary/common/generator/helper/CommonPropertiesHelper.java
@@ -9,23 +9,33 @@ public class CommonPropertiesHelper {
 	public static String COMMON_PROPERTIES_PATH = "src/main/resources/common.properties";
 	public static String PROPERTY_GENERATE_JPA = "generate.jpa";
 	public static String PROPERTY_GENERATE_BVA = "generate.bva";
+	public static String PROPERTY_IMPL_GENERALIZATION = "impl.generalization";
+	public static String PROPERTY_NOT_JPA_CLASS = "not.entity.class";
 	
 	public static String getJpaFlag() throws FileNotFoundException, IOException {
 		Properties properties = new Properties();
 		properties.load(new FileInputStream(COMMON_PROPERTIES_PATH));
 		return properties.getProperty(PROPERTY_GENERATE_JPA);
 	}
+
 	public static String getBvaFlag() throws FileNotFoundException, IOException {
 		Properties properties = new Properties();
 		properties.load(new FileInputStream(COMMON_PROPERTIES_PATH));
 		return properties.getProperty(PROPERTY_GENERATE_BVA);
 	}
-	
 
-	public static String PROPERTY_IMPL_GENERALIZATION = "impl.generalization";
-	public static String implHierarchy() throws FileNotFoundException, IOException {
+	public static String implHierarchy() throws FileNotFoundException,
+			IOException {
 		Properties properties = new Properties();
 		properties.load(new FileInputStream(COMMON_PROPERTIES_PATH));
 		return properties.getProperty(PROPERTY_IMPL_GENERALIZATION);
 	}
+
+	public static String generateNotEntity() throws FileNotFoundException,
+			IOException {
+		Properties properties = new Properties();
+		properties.load(new FileInputStream(COMMON_PROPERTIES_PATH));
+		return properties.getProperty(PROPERTY_NOT_JPA_CLASS);
+	}
+
 }
